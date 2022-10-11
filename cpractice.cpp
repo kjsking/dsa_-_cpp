f7609f70b1777ecd7e4ffa4931e0e989fbc9bd89
kjsking <115573345+kjsking@users.noreply.github.com>
2022-10-11 22:05:38 +0530

    Add files via upload

---
+++ cpractice.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+//	unsigned int f = -65;
+//	printf("%d\n",f);
+//		printf("%u",f);
+		
+//		unsigned char f = -65;
+//	printf("%d\n",f);
+//		printf("%u",f);
+//		
+
+
+unsigned int a = 100;
+int b = -1;
+if(a>b)
+printf("a is roc k");
+else 
+printf("b is rock");
+		
+//	int a = 30,b=40;
+//	unsigned int d = a*b;
+//		printf("%d\n",d);
+//	printf("%d\n",a*b);
+//	printf("%u\n",d);
+}
---
+++ increaments.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+	int a = 5;
+	int c = ++a++;
+	printf("%d",a);
+		printf("%d",c);
+}
